4bee7b603a112d1e45927814af5b96c859ad4ec3
RApostolJR <romuloapostol@gmail.com>
2024-03-14 22:06:45 -0700

    created html for bmi and food, updated variables in javascript code, ensured javascript functionality for second row which includes food input, created two separate local storage functions for BMI to FOOD

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,44 +7,48 @@ var trackerBmiAgeEl = $('#age-input-bmi');
 var trackerBmiGenderEl = $('#gender-input-bmi');
 var trackerBmiHeightEl = $('#height-input-bmi');
 var trackerBmiWeightEl = $('#weight-input-bmi');
-var trackerBmiDateInputEl = $('date-input-bmi');
+var trackerBmiDateInputEl = $('#date-input-bmi');
 
+// DISPLAYS TIME
 function displayTime() {
-  var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
-  timeDisplayEl.text(rightNow);
+  var currentTime = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
+  timeDisplayEl.text(currentTime);
 }
 
-// Javascript for BMI inputs (first row)
-function readProjectsFromStorage() {
+// BMI INPUT SECTION
 
-  var projects = localStorage.getItem('projects');
-  if (projects) {  
-    projects = JSON.parse(projects);    
+// SAVES BMI TO LOCAL STORAGE; bmiLocalStorage, saveAllBmiDataToStorage
+function bmiLocalStorage() {
+  var allBmiData = localStorage.getItem('allBmiData');
+  if (allBmiData) {  
+    allBmiData = JSON.parse(allBmiData);    
   } else {
-    projects = [];
+    allBmiData = [];
   }
-  return projects;
+  return allBmiData;
 }
 
-function saveProjectsToStorage(projects) {
-  localStorage.setItem('projects', JSON.stringify(projects));  
+function saveAllBmiDataToStorage(allBmiData) {
+  localStorage.setItem('allBmiData', JSON.stringify(allBmiData));  
 }
 
-function printProjectData() {
+// PRINTS BMI TO TABLE
+function printBmiData() {
   trackerDisplayBmiEl.empty();
-  var projects = readProjectsFromStorage();
+  var allBmiData = bmiLocalStorage();
 
-  for (var i = 0; i < projects.length; i += 1) {
-    var project = projects[i];  
-    var projectDate = dayjs(bmi.date); 
+  // CREATES ROW CONTENT FOR BMI DATA
+  for (var i = 0; i < allBmiData.length; i += 1) {
+    var bmi = allBmiData[i];  
+    var bmiDate = dayjs(bmi.date); 
     var today = dayjs().startOf('day');  
 
     var rowEl = $('<tr>');  
-    var ageEL = $('<td>').text(project.age);
-    var genderEl = $('<td>').text(project.gender);
-    var heightEl = $('<td>').text(project.height);
-    var weightEl = $('<td>').text(project.weight);
-    var dateEl = $('<td>').text(projectDate.format('MM/DD/YYYY'));
+    var ageEL = $('<td>').text(bmi.age);
+    var genderEl = $('<td>').text(bmi.gender);
+    var heightEl = $('<td>').text(bmi.height);
+    var weightEl = $('<td>').text(bmi.weight);
+    var dateEl = $('<td>').text(bmiDate.format('MM/DD/YYYY'));
 
     var deleteEl = $(
       '<td><button class="btn btn-sm btn-delete-project" data-index="' +
@@ -52,9 +56,10 @@ function printProjectData() {
         '">X</button></td>'
     );
 
-    if (projectDate.isBefore(today)) {
+    // COLOR CODE FOR CURRENT DATE VS PAST DATE
+    if (bmiDate.isBefore(today)) {
       rowEl.addClass('project-late');
-    } else if (projectDate.isSame(today)) {
+    } else if (bmiDate.isSame(today)) {
       rowEl.addClass('project-today');
     }
   
@@ -63,16 +68,18 @@ function printProjectData() {
   }
 }
 
-function handleDeleteProject() {
+// DELETES BMI INFO WHEN 'X' IS PRESSED AND REMOVES FROM LOCAL STORAGE
+function handleDeleteBmi() {
   var projectIndex = parseInt($(this).attr('data-index'));
-  var projects = readProjectsFromStorage();
-  projects.splice(projectIndex, 1);
-  saveProjectsToStorage(projects);
+  var allBmiData = bmiLocalStorage();
+  allBmiData.splice(projectIndex, 1);
+  saveAllBmiDataToStorage(allBmiData);
 
-  printProjectData();
+  printBmiData();
 }
 
-function handleProjectFormSubmit(event) {
+// BMI MODAL SUBMISSION
+function handleBmiFormSubmit(event) {
   event.preventDefault();  
 
   var bmiAge = trackerBmiAgeEl.val().trim();     
@@ -89,12 +96,12 @@ var bmiData = {
     date: bmiDate,
   };
 
-  var bmiDataList = readProjectsFromStorage();
+  var bmiDataList = bmiLocalStorage();
 
   bmiDataList.push(bmiData);
-  saveProjectsToStorage(bmiDataList);
+  saveAllBmiDataToStorage(bmiDataList);
 
-  printProjectData();
+  printBmiData();
 
   trackerBmiAgeEl.val('');
   trackerBmiGenderEl.val('');
@@ -103,21 +110,104 @@ var bmiData = {
   trackerBmiDateInputEl.val('');
 }
 
-trackerFormBmiEl.on('submit', handleProjectFormSubmit);
+trackerFormBmiEl.on('submit', handleBmiFormSubmit);
+trackerDisplayBmiEl.on('click', '.btn-delete-project', handleDeleteBmi);
+printBmiData();
 
-trackerDisplayBmiEl.on('click', '.btn-delete-project', handleDeleteProject);
+// FOOD INPUT SECTION
+
+var displayFoodEl = $('#display-food');
+var formFoodEl = $('#form-food');
+var inputFoodEl = $('#input-food');
+var dateInputFoodEl = $('#date-input-food');
+
+// SAVES FOOD TO LOCAL STORAGE; foodLocalStorage, saveAllFoodDataToStorage
+function foodLocalStorage() {
+
+  var allFoodData = localStorage.getItem('allFoodData');
+  if (allFoodData) {  
+    allFoodData = JSON.parse(allFoodData);    
+  } else {
+    allFoodData = [];
+  }
+  return allFoodData;
+}
+
+function saveAllFoodDataToStorage(allFoodData) {
+  localStorage.setItem('allFoodData', JSON.stringify(allFoodData));  
+}
+
+// PRINTS FOOD TO TABLE
+function printFoodData() {
+  displayFoodEl.empty();
+  var allFoodData = foodLocalStorage();
+
+  // CREATES ROW CONTENT FOR FOOD DATA
+  for (var i = 0; i < allFoodData.length; i += 1) {
+    var foods = allFoodData[i];  
+    var foodsDate = dayjs(foods.date); 
+    var today = dayjs().startOf('day');  
+
+    var secondRowEl = $('<tr>');  
+    var foodSelectionEl = $('<td>').text(foods.foodSelection);
+    // var foodCalorieEl = $('<td>').text(foods.foodCalorie);
+    var foodDateEl = $('<td>').text(foodsDate.format('MM/DD/YYYY'));
+
+    var secondDeleteEl = $(
+      '<td><button class="btn btn-sm btn-delete-project" data-index="' +
+        i +
+        '">X</button></td>'
+    );
+
+    // COLOR CODE FOR CURRENT DATE VS PAST DATE
+    if (foodsDate.isBefore(today)) {
+      secondRowEl.addClass('project-late');
+    } else if (foodsDate.isSame(today)) {
+      secondRowEl.addClass('project-today');
+    }
+  
+    // Commented out calorie table unit information for API calorie
+    secondRowEl.append(foodSelectionEl, /*foodCalorieEl,*/ foodDateEl, secondDeleteEl);
+    displayFoodEl.append(secondRowEl);
+  }
+}
+
+// DELETES FOOD INFO WHEN 'X' IS PRESSED AND REMOVES FROM LOCAL STORAGE
+function handleDeleteFood() {
+  var projectIndex = parseInt($(this).attr('data-index'));
+  var allFoodData = foodLocalStorage();
+  allFoodData.splice(projectIndex, 1);
+  saveAllFoodDataToStorage(allFoodData);
+
+  printFoodData();
+}
+
+// FOOD MODAL SUBMISSION
+function handleFoodFormSubmit(event) {
+  event.preventDefault();  
+
+  var ingredient = inputFoodEl.val().trim();
+  var foodsDate = dateInputFoodEl.val(); 
+
+var foodData = {     
+    foodSelection: ingredient,
+    date: foodsDate,
+  };
+
+  var foodDataList = foodLocalStorage();
+
+  foodDataList.push(foodData);
+  saveAllFoodDataToStorage(foodDataList);
+
+  printFoodData();
+
+  inputFoodEl.val('');
+  dateInputFoodEl.val('');
+}
+
+formFoodEl.on('submit', handleFoodFormSubmit);
+displayFoodEl.on('click', '.btn-delete-project', handleDeleteFood);
+printFoodData();
 
 displayTime();
 setInterval(displayTime, 1000);  
-
-printProjectData();
-
-
-/*
-
-var projectDisplayFoodEl = $('#project-display-food');
-var projectFormFoodEl = $('#project-form-food');
-var projectInputFoodEl = $('#input-food');
-var projectDateInputFoodEl = $('#project-date-input-food');
-
-*/
